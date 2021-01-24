302a337fd620ebce4d2e3638789e17e42db730f1
pankaj-889 <71728143+pankaj-889@users.noreply.github.com>
2021-01-24 18:56:19 +0530

    Day 2 assignment 2

---
+++ a.js
@@ -0,0 +1,13 @@
+//question 1
+const clothes = ['jacket', 't-shirt'];
+clothes.length = 0;
+console.log(clothes[0]); 
+
+// question 2
+var arr=[1,2,3,4,5];
+var sum=0;
+for(var num=0;num<arr.length ;num++)
+{
+    sum+=arr[num];
+}
+console.log(sum);
